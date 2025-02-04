596e0ec16c35cd7db8163cb361573f84d68e12f6
jacob-cantrell <jcantrell218@gmail.com>
2025-02-04 18:56:41 -0500

    added two lines for functionality testing

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from graphics import Window, Point, Line
 
 def main():
     win = Window(800, 600)
+    line1 = Line(Point(0, 0), Point(50, 50)) 
+    line2 = Line(Point(0, 100), Point(100, 0))
+    win.draw_line(line1, "black")
+    win.draw_line(line2, "black")
     win.wait_for_close()
 
 main()
