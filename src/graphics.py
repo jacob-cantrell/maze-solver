44c49bea2a0a240cbf0506ef7694d5975ea07fdb
jacob-cantrell <jcantrell218@gmail.com>
2025-02-05 10:57:02 -0500

    added Maze class, updated main for testing, and added unittests for testing in tests.py

--- src/graphics.py
+++ src/graphics.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from time import sleep
 
 class Window():
     def __init__(self, width, height):
@@ -80,3 +81,50 @@ class Cell:
             self._win.draw_line(line, "red")
         else:
             self._win.draw_line(line, "gray")
+
+class Maze():
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win=None,
+    ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._cells = []
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col_cells = []
+            for j in range(self._num_rows):
+                col_cells.append(Cell(self._win))
+            self._cells.append(col_cells)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cells(i, j)
+
+    def _draw_cells(self, i, j):
+        if self._win is None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win is None:
+            return
+        self._win.redraw()
+        sleep(0.05)
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
-from graphics import Window, Cell
+from graphics import Window, Maze
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(win)
-    cell2 = Cell(win)
-    cell1.draw(450, 450, 600, 600)
-    cell2.draw(150, 150, 250, 250)
-    cell1.draw_move(cell2, False)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    cell_size_x = (800 - 2 * margin) / num_cols
+    cell_size_y = (600 - 2 * margin) / num_rows
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
     win.wait_for_close()
 
 main()
